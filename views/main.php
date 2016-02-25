70cc33ee441d1bf3e8200fcf5eeb8f170a1dada3
Igor01 <Ih01@i.ua>
2016-02-25 19:03:41 +0200

    One menu for all docs

--- views/main.php
+++ views/main.php
@@ -1,37 +1,4 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
-        <title>9-Б класс СЗСШ№1</title>
-    </head>
-    <body>
-        <heder>
-            <h1>9-Б класс СЗСШ№1</h1>
-            <ul id="navbarH">
-                <li><a href="#">Главная</a></li>
-                <li><a href="#">Альбом</a></li>
-                <li><a href="#">Обьявления</a></li>
-                <li><a href="#">Чат</a></li>
-            </ul>
-        </heder>
-        <nav>
-            <ul id="navbarV">
-                <li><a href="#">Главная</a></li>
-                <li><a href="#">Альбом</a></li>
-                <li><a href="#">Обьявления</a></li>
-                <li><a href="#">Чат</a></li>
-            </ul>
-        </nav>
-        <article>
-            article
-        </article>
-        <footer>
-            <i>Copyright &copy; 2016</i>
-            <br>
-            <b>By IMC</b>
-        </footer>
-    </body>
+    article
 </html>
